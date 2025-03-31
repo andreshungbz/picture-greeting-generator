b5c668d1d7c8ae6f8f7ec7fdc1768f916c980675
Andres Hung <2018118240@ub.edu.bz>
2025-03-31 10:27:50 -0600

    Add Promise chaining version of function

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,6 +6,7 @@ const greeting = document.querySelector('#greeting');
 const author = document.querySelector('#author');
 const generateBtn = document.querySelector('#generate-button');
 
+// version using async/await syntax
 const changePictureGreeting = async () => {
   const response = await fetch('/generate');
   const data = await response.json();
@@ -13,6 +14,17 @@ const changePictureGreeting = async () => {
   image.setAttribute('src', data.imgPath);
   greeting.textContent = data.greeting.text;
   author.textContent = data.greeting.author;
+};
+
+// version using Promise chaining
+const changePictureGreetingChain = () => {
+  fetch('/generate')
+    .then((response) => response.json())
+    .then((data) => {
+      image.setAttribute('src', data.imgPath);
+      greeting.textContent = data.greeting.text;
+      author.textContent = data.greeting.author;
+    });
 };
 
 generateBtn.addEventListener('click', changePictureGreeting);
